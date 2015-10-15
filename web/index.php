98e8570e0e7eb896a0f82ef02099f4149ca4dea7
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-10-15 23:48:26 +0200

    First examples

---
+++ web/index.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true; //debug mode
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return sprintf('Hello %s!', $app->escape($name));
+});
+
+$blogPosts = array(
+    1 => array(
+        'date'      => '2011-03-29',
+        'author'    => 'igorw',
+        'title'     => 'Using Silex',
+        'body'      => '...',
+    ),
+);
+
+$app->get('/blog', function () use ($blogPosts) {
+    $output = '';
+    foreach ($blogPosts as $post) {
+        $output .= $post['title'];
+        $output .= '<br />';
+    }
+
+    return $output;
+});
+
+$app->get('/blog/{id}', function (Silex\Application $app, $id) use ($blogPosts) {
+    if (!isset($blogPosts[$id])) {
+        $app->abort(404, "Post $id does not exist.");
+    }
+
+    $post = $blogPosts[$id];
+
+    return  "<h1>{$post['title']}</h1>".
+    "<p>{$post['body']}</p>";
+});
+
+$app->run();
